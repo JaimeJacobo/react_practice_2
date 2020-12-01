4f75baa8737f4a66607c981d9f31046b6bb59429
Jaime Jacobo <jaimejacobordv@gmail.com>
2020-12-01 11:06:56 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Cities from './components/Cities'
+import Pets from './components/Pets'
+
+class App extends React.Component {
+
+  state = {
+    username: 'jaimejacobo',
+    email: 'jaime@jaime.com',
+    amountOfMoney: 50,
+    admin: true,
+    cities: ['Madrid', 'Granada', 'Sevilla', 'Barcelona', 'Lugo'],
+    alignment: 'center',
+    changeAlignment: () => {
+      this.state.alignment === 'center' 
+        ? this.setState({alignment: 'left'}) 
+        : this.setState({alignment: 'center'})
+      },
+    pets: [
+      {animal: 'dog', name: 'Maya'},
+      {animal: 'dog', name: 'Jordi'},
+      {animal: 'cat', name: 'Misifu'},
+      {animal: 'horse', name: 'Firulais'}
+    ],
+    deletePet: (_name)=>{
+      const newPets = this.state.pets.filter((pet)=>{
+        return pet.name !== _name
+      })
+      this.setState({pets: newPets})
+    },
+    addPet: (_pet) => {
+
+      // this.setState({pets: [{animal: 'dog', name: _pet}, ...this.state.pets]})
+      
+      const copyOfPets = [...this.state.pets]
+      copyOfPets.unshift({animal: 'dog', name: _pet})
+      this.setState({pets: copyOfPets})
+    }
+  }
+
+  changeUsername = () => {
+    this.state.username === 'jaimejacobo' 
+      ? this.setState({username: 'pedrovargas', amountOfMoney: 50}) 
+      : this.setState({username: 'jaimejacobo', amountOfMoney: 50})
+  }
+
+  changeAmountOfMoney = (_operation) => {
+    if(_operation === 'add'){
+      this.setState({amountOfMoney: this.state.amountOfMoney + 10})
+    } else if(_operation === 'substract'){
+      this.setState({amountOfMoney: this.state.amountOfMoney - 10})
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+        <h1>Main page</h1>
+        {this.state.admin && <h2>WELCOME ADMIN!!!</h2>}
+
+        <h2>Hello, {this.state.username}</h2>
+        <button onClick={()=>this.changeUsername()}>Change username</button>
+
+        <p>Money: {this.state.amountOfMoney}$</p>
+        <button onClick={()=>this.changeAmountOfMoney('add')}>Add 10$</button>
+        <button onClick={()=>this.changeAmountOfMoney('substract')}>Remove 10$</button>
+
+        <Cities 
+          cities={this.state.cities} 
+          alignment={this.state.alignment} 
+          changeAlignment={this.state.changeAlignment}
+        />
+
+        <Pets pets={this.state.pets} deletePet={this.state.deletePet} addPet={this.state.addPet}/>
+
+      </div>
+    );    
+  }
+} 
 
 export default App;
---
+++ src/components/Cities.js
@@ -0,0 +1,27 @@
+
+import React from 'react'
+
+const Cities = (props) => {
+
+  const renderCities = () => {
+    const cities = props.cities.map((city, index) => <li key={index}>{city}</li>)
+    return cities
+  }
+
+  const changeAlignment = () => {
+    props.changeAlignment()
+  }
+
+  return(
+    <div>
+      <h2>All my cities</h2>
+      <button onClick={()=>changeAlignment()}>Change text-align</button>
+      <ul style={{textAlign: props.alignment}}>
+        {renderCities()}
+      </ul>
+    </div>
+  )
+}
+
+export default Cities
+
